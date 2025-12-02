48948ffaa7cbafee879132bb79fb8e6797d74ff2
mukulchaudhatu <chaudharymukul1430@gmail.com>
2025-12-02 19:29:44 +0530

    for predict

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import numpy as np
+import pickle
+
+app = FastAPI()
+
+# Load ML model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+# HTML templates folder
+templates = Jinja2Templates(directory="templates")
+
+# CSS/Static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/predict")
+def predict(request: Request, area: float = Form(...)):
+    area_val = np.array([[area]])
+    prediction = model.predict(area_val)[0]
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "area": area,
+            "result": round(float(prediction), 2)
+        }
+    )
